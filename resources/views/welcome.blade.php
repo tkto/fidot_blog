dc7ab75fef7aa8030006fba57da2f66958f3100e
shakib <test@gmail.com>
2016-10-19 16:13:32 +0600

    first commit fidot

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@include('partials._head')
+
+@include('partials.nav')
+  @yield('content')
+<div class="container">
+  <h3>Right Aligned Navbar</h3>
+  <p>The .navbar-right class is used to right-align navigation bar buttons.</p>
+</div>
+
+</body>
+</html>
+
